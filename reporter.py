e4af6149052e6b6f8bf06ac75b1ed55c530c551b
moshA-B <burtonmoshe@gmail.com>
2026-02-11 19:25:21 +0200

    added function to match ports with the right protocol

--- reporter.py
+++ reporter.py
@@ -8,3 +8,8 @@ def count_calls(data):
         times[log[1]] = times.get(log[1], 0) + 1
     return times
 
+
+#returns port nums : protocol
+def map_port_numbers(data):
+    proto_numbers={int(log[3]):log[4] for log in data}
+    return proto_numbers
